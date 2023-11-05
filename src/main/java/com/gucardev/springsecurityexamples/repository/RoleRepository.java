a6fd901c24bf962d6bb30d3ea2765fdbf9ce0114
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-11-05 16:10:24 +0300

    feat: in memory authentication example created

--- src/main/java/com/gucardev/springsecurityexamples/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.gucardev.springsecurityexamples.repository;
-
-import com.gucardev.springsecurityexamples.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {}
