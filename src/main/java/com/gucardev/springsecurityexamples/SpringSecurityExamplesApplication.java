baa1e4662c6b70df8b811f7ab99f27ca7d915562
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-11-05 13:14:11 +0300

    feat: user and authority entities created

--- src/main/java/com/gucardev/springsecurityexamples/SpringSecurityExamplesApplication.java
+++ src/main/java/com/gucardev/springsecurityexamples/SpringSecurityExamplesApplication.java
@@ -2,12 +2,13 @@ package com.gucardev.springsecurityexamples;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringSecurityExamplesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurityExamplesApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(SpringSecurityExamplesApplication.class, args);
+  }
 }
---
+++ src/main/java/com/gucardev/springsecurityexamples/model/User.java
@@ -0,0 +1,46 @@
+package com.gucardev.springsecurityexamples.model;
+
+import jakarta.persistence.*;
+import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+@Table(name = "USERS")
+@Getter
+@Setter
+public class User extends BaseEntity implements UserDetails {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column(nullable = false)
+  private String username;
+
+  @Column(nullable = false)
+  private String password;
+
+  @Column(name = "account_non_expired", columnDefinition = "boolean default true")
+  private boolean accountNonExpired;
+
+  @Column(name = "is_enabled", columnDefinition = "boolean default true")
+  private boolean isEnabled;
+
+  @Column(name = "account_non_locked", columnDefinition = "boolean default true")
+  private boolean accountNonLocked;
+
+  @Column(name = "credentials_non_expired", columnDefinition = "boolean default true")
+  private boolean credentialsNonExpired;
+
+  @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+  @JoinTable(
+      name = "user_role",
+      joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+      inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+  private Set<Role> authorities;
+}
