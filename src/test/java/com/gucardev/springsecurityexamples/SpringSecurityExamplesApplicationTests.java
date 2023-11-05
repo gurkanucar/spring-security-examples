2b00bd1949514da8607eb251fa326576d58d614d
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-11-05 12:58:17 +0300

    Initial commit

---
+++ src/test/java/com/gucardev/springsecurityexamples/SpringSecurityExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gucardev.springsecurityexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
