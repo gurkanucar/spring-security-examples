fcc0eb92da5c9272e3ecd463564c6eebe9a3959c
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-11-05 15:51:17 +0300

    feat: SecurityConfig created

---
+++ src/main/java/com/gucardev/springsecurityexamples/security/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.gucardev.springsecurityexamples.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+    httpSecurity
+        .headers(x -> x.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+        .csrf(AbstractHttpConfigurer::disable)
+        .cors(Customizer.withDefaults())
+        .formLogin(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(x -> x.anyRequest().permitAll())
+        .httpBasic(Customizer.withDefaults());
+    return httpSecurity.build();
+  }
+}
