22595b3505320cc7a5d4c7dad0cdc511ffc32294
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-11-05 16:24:03 +0300

    refactor: small bugs fixed

--- src/main/java/com/gucardev/springsecurityexamples/controller/PrivateTestController.java
+++ src/main/java/com/gucardev/springsecurityexamples/controller/PrivateTestController.java
@@ -1,6 +1,11 @@
+
+
+
+
 package com.gucardev.springsecurityexamples.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,11 +19,13 @@ public class PrivateTestController {
     return ResponseEntity.ok("Hello Any Authenticated! This is private endpoint");
   }
 
+  @PreAuthorize("hasRole('ROLE_ADMIN')")
   @GetMapping("/admin")
   public ResponseEntity<String> sayHelloToAdmin() {
     return ResponseEntity.ok("Hello Admin! This is private endpoint");
   }
 
+  @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_USER')")
   @GetMapping("/user")
   public ResponseEntity<String> sayHelloToUser() {
     return ResponseEntity.ok("Hello User! This is private endpoint");
--- src/main/java/com/gucardev/springsecurityexamples/security/SecurityConfig.java
+++ src/main/java/com/gucardev/springsecurityexamples/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.gucardev.springsecurityexamples.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -13,6 +14,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
   @Bean
